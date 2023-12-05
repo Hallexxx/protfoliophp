4b3e99e9d641e47fa2d39775bb582449f823b3c1
Hallexxx <112977841+Hallexxx@users.noreply.github.com>
2023-12-05 17:44:16 +0100

    sasas

--- index.php
+++ index.php
@@ -3,9 +3,28 @@ include('includes/header.php');
 ?>
 
 <section id="home">
-    <div class="intro scroll-animation">
-        <h1>Bienvenue sur mon Portfolio</h1>
-        <p>Je suis Alexandre Perez, un développeur très incroyablement passionné.</p>
+    <div class="container scroll-animation">
+        <div class="image-container">
+            <img src="/perez.jpg" alt="Image" class="rounded-image">
+            <div class="overlay-text">
+                <p> 
+                Nom : Perez <br>
+                Prénom : Alexandre <br>
+                Téléphone : 0667619382<br>
+                Etude : Bachelor 1 Informatique <br>
+                E-mail : alex.perezab470@gmail.com <br>
+                Baccalauréat : Baccalauréat professionnel SN Mention Bien</p>
+            </div>
+        </div>
+        <div class="pres">
+            <div class="text">
+                <h1>
+                    Bienvenue<br>
+                    Je m'appelle <span id="test">Alexandre Perez</span>, je suis actuellement en <em>deuxieme</em> année de Bachelor <em>Informatique</em> a Ynov.<br>
+                    J'aime le développement WEB <em>Front/End</em> et le développement Jeu Vidéo notamment sur <em>Unity</em> que j'ai dejà pratiquer.
+                </h1>
+            </div>
+        </div>
     </div>
 
     <div class="skills scroll-animation">
