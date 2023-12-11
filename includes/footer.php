b420245da303d10c043bcc854bf31ab5ade0fd09
Hallexxx <112977841+Hallexxx@users.noreply.github.com>
2023-12-11 17:22:27 +0100

    sqdqdqd

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,13 @@
-<footer>
-        <p>&copy; <?php echo date("Y"); ?> Alexandre Perez</p>
-    </footer>
-</body>
+        <footer>
+            <div class="footer-col">
+                <div class="social-links">
+                    <a href="https://twitter.com/HallexxP"><i class="fab fa-twitter"></i></a>
+                    <a href="https://github.com/Hallexxx"><i class="fab fa-github"></i></a>
+                    <a href="https://www.instagram.com/hallexx__/"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/alexandre-perez-b3309b267/"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <p>© 2023 Alexandre Perez - Tous droits réservés</p>
+        </footer>
+    </body>
 </html>
