07d3f9641d176ada31b958738117876c7ce0a749
Hallexxx <112977841+Hallexxx@users.noreply.github.com>
2023-12-15 13:58:28 +0100

    fezzfz

--- animation.js
+++ animation.js
@@ -124,4 +124,10 @@ function isElementInViewport(el) {
     );
 }
 
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
 
--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Portfolio</title>
-        <link rel="stylesheet" href="/style.css">
+        <link rel="stylesheet" href="/css/style.css">
         <script src="https://unpkg.com/gsap@3.9.0/dist/gsap.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
         <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
